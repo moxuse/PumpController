9edd365a62a49142907bba1c94b8eb0a9c4ba9bc
moxus <moxuse@gmail.com>
2013-01-22 14:15:18 +0900

    gitignore

---
+++ src/testApp.cpp
@@ -0,0 +1,161 @@
+#include "testApp.h"
+
+#define RECIVE_PORT 5000
+#define BAUD_RATE 9600
+
+//--------------------------------------------------------------
+void testApp::setup(){
+    run = true;
+    
+    serial.enumerateDevices();
+    serial.setup( "/dev/tty.usbmodem1411", BAUD_RATE );
+    
+    reciver.setup( RECIVE_PORT );
+    
+    timer.setup(1000, true);
+    
+    gui = new ofxUICanvas();
+    
+    gui->addWidgetDown(new ofxUILabel("Pomp Controller for yebizo 2013", OFX_UI_FONT_LARGE));
+    gui->addToggle("Recive osc and run", true, 30, 30);
+    gui->addButton("Reset", isReset, 30, 30);
+    gui->addLabel("CurrentLevel", "CurrentLevel : 0");
+    ofAddListener(gui->newGUIEvent,this,&testApp::guiEvent);
+    
+    timer.startTimer();
+    ofAddListener( timer.TIMER_REACHED, this, &testApp::timeerCallback );
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+    
+    while ( reciver.hasWaitingMessages() ) {
+        ofxOscMessage m;
+        reciver.getNextMessage( &m );
+        
+        if( "/recive" == m.getAddress() ) {
+            int nextLevel = m.getArgAsInt32(0);
+            proceedLevel( nextLevel );
+            if( !run ){
+                cout << "recived OSC message but not running flag.." << endl;
+            }
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+    
+    ofBackground(0);
+    
+}
+
+//--------------------------------------------------------------
+void testApp::timeerCallback( ofEventArgs &e ) {
+    int count;
+    count = timer.count;
+    cout << "timer recieved _________ " << &e << " ms : " << count << endl;
+}
+
+//--------------------------------------------------------------
+void testApp::guiEvent( ofxUIEventArgs &e ){
+    string name = e.widget->getName();
+    int kind = e.widget->getKind();
+    
+    if( "Recive osc and run" == name ){
+        ofxUIToggle *tog = (ofxUIToggle *) e.widget;
+        run = tog->getValue();
+        
+//        if( !run ){
+//            timer.pauseTimer();
+//        } else {
+//            timer.resumeTimer();
+//        }
+        
+    } else if ( "Reset" == name ){
+        reset();
+    }
+}
+
+//--------------------------------------------------------------
+void testApp::proceedLevel( int _nextLevel ) {
+    if( run ){
+        int next = _nextLevel;
+        if( 9999 != next ){
+            
+            if( currntLevel > next ){
+                unsigned char buf[3] = "XB";
+                serial.writeBytes( buf , sizeof(buf) / sizeof(buf[0]) );
+            } else {
+                unsigned char buf[3] = "XF";
+                serial.writeBytes( buf , sizeof(buf) / sizeof(buf[0]) );
+            }
+            
+            timer.startTimer();
+            
+        } else {
+            
+            unsigned char buf[3] = "XR";
+            serial.writeBytes( buf , sizeof(buf) / sizeof(buf[0]) );
+        }
+    }
+};
+
+//--------------------------------------------------------------
+void testApp::reset() {
+    
+    //timer.stopTimer();
+    
+    timer.reset();
+    timer.setTimer( ofRandom(1200) );
+    run = false;
+};
+
+
+
+
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
---
+++ src/testApp.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxUI.h"
+#include "ofxOsc.h"
+#include "ofxTimer.h"
+
+class testApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed  (int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+    
+		void gotMessage(ofMessage msg);
+        void timeerCallback( ofEventArgs &e );
+        void guiEvent( ofxUIEventArgs &e );
+        void proceedLevel( int _nextLevel );
+        void reset();
+		
+    private:
+        ofxUICanvas *gui;
+        ofSerial serial;
+        ofxOscReceiver reciver;
+        ofxTimer timer;
+        bool run;
+        bool isReset;
+        int currntLevel;
+    
+};
